078ba76431d861e71b62f31d1f6d471308ab0f87
Kyu Lee <kwooler@gmail.com>
2023-05-21 12:00:19 -0700

    Assignment 2-1

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def main():
+    """
     ##################################################
-    # Comlete your code here
+    Comlete your code here
+    Use m_perc and f_perc for your results
     ##################################################
-    pass
+    """
+
+    """
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    """
+    return m_perc, f_perc
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,23 +10,19 @@ def test_main_1():
     datastr = '40 \n 60'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    m_perc, f_perc = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*100[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
-
-    res = re.search(r'[\w,\W]*40[\w,\W]*60[\w,\W]*', lines[1])
-    assert res != None
-    print(res.group())
-
-    res = re.search(r'[\w,\W]*40\.00[\w,\W]*60\.00[\w,\W]*', lines[2])
-    assert res != None
-    print(res.group())
+    # res = re.search(r'[\w,\W]*100[\w,\W]*', lines[0])
+    # assert res != None
+    # print(res.group())
+    m = f'{m_perc:.2f}'
+    f = f'{f_perc:.2f}'
+    assert m == '40.00', 'Expect 40.00'
+    assert f == '60.00', 'Expect 40.00'
 
 
 def test_main_2():
@@ -35,20 +31,16 @@ def test_main_2():
     datastr = '10 \n 20'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    m_perc, f_perc = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*30[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
-
-    res = re.search(r'[\w,\W]*10[\w,\W]*20[\w,\W]*', lines[1])
-    assert res != None
-    print(res.group())
-
-    res = re.search(r'[\w,\W]*33\.33[\w,\W]*66\.67[\w,\W]*', lines[2])
-    assert res != None
-    print(res.group())
+    # res = re.search(r'[\w,\W]*30[\w,\W]*', lines[0])
+    # assert res != None
+    # print(res.group())
+    m = f'{m_perc:.2f}'
+    f = f'{f_perc:.2f}'
+    assert m == '33.33', 'Expect 33.33'
+    assert f == '66.67', 'Expect 66.67'
