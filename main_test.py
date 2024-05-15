03dd917048c49d3240b58dbc0cfcf1d5c082bae3
Kyuwoong Lee <kwooler@gmail.com>
2024-05-14 18:34:48 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -7,10 +7,10 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '40 \n 60'
+    datastr = '40 \n 40 \n 20'
     sys.stdin = io.StringIO(datastr)
 
-    m_perc, f_perc = main.main()
+    m_perc, f_perc, nb_perc = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
@@ -21,17 +21,19 @@ def test_main_1():
     # print(res.group())
     m = f'{m_perc:.2f}'
     f = f'{f_perc:.2f}'
+    nb = f'{nb_perc:.2f}'
     assert m == '40.00', 'Expect 40.00'
-    assert f == '60.00', 'Expect 40.00'
+    assert f == '40.00', 'Expect 40.00'
+    assert nb == '20.00', 'Expect 20.00'
 
 
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '10 \n 20'
+    datastr = '10 \n 20 \n 30'
     sys.stdin = io.StringIO(datastr)
 
-    m_perc, f_perc = main.main()
+    m_perc, f_perc, nb_perc = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
@@ -42,5 +44,7 @@ def test_main_2():
     # print(res.group())
     m = f'{m_perc:.2f}'
     f = f'{f_perc:.2f}'
-    assert m == '33.33', 'Expect 33.33'
-    assert f == '66.67', 'Expect 66.67'
+    nb = f'{nb_perc:.2f}'
+    assert m == '16.67', 'Expect 16.67'
+    assert f == '33.33', 'Expect 33.33'
+    assert nb == '50.00', 'Expect 50.00'
