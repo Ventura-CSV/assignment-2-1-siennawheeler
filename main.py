e94595e46cf8cce2d2232e5780a43bde90d1380f
siennawheeler <sienna_wheeler1@my.vcccd.edu>
2024-06-18 09:10:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ def main():
     Use m_perc, f_perc, nb_perc for your results
     ##################################################
     """
+    
+    m_num = int(input("Number of males: ")
+    f_num = int(input("Nuumber of females: ")
+    nb_num = int(input("Number of non-binary people: ")
+
+    total = m_num + f_num + nb_num
+    m_perc = m_num * 100 / total
+    f_perc = f_num * 100 / total
+    nb_perc = nb_num * 100 / total
+
     """
     ########################################
     # Do not delete the return statement
